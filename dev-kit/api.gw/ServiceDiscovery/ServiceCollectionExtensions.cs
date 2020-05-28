76b5ead02da34e241dcd234c279628ff86892dd4
s.azarin <azarin.sergey@gmail.com>
2020-05-28 17:25:19 +0700

    initial

---
+++ dev-kit/api.gw/ServiceDiscovery/ServiceCollectionExtensions.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using Consul;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Api.Gw.ServiceDiscovery
+{
+    public static class ServiceCollectionExtensions
+    {
+        private static string _host;
+
+        public static IServiceCollection AddConsul(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<ConsulConfig>(configuration.GetSection("ServiceConfig"));
+            services.AddSingleton<IConsulClient, ConsulClient>(p => new ConsulClient(consulConfig =>
+            {
+                var host = configuration["ServiceConfig:serviceDiscoveryAddress"];
+                _host = host;
+                consulConfig.Address = new Uri(host);
+            }));
+            return services;
+        }
+
+        public static IApplicationBuilder UseConsul(this IApplicationBuilder app)
+        {
+            const string healthCheckPath = "/tool/health/ping";
+            app.UseRouter(builder =>
+            {
+                builder.MapGet(healthCheckPath, context => context.Response.WriteAsync("OK"));
+            });
+
+            var consulClient = app.ApplicationServices.GetRequiredService<IConsulClient>();
+            var consulConfig = app.ApplicationServices.GetRequiredService<IOptions<ConsulConfig>>();
+            var logger = app.ApplicationServices.GetRequiredService<ILoggerFactory>().CreateLogger("Consul.AspNetCore");
+            var lifetime = app.ApplicationServices.GetRequiredService<IHostApplicationLifetime>();
+
+            var uri = new Uri(consulConfig.Value.ServiceAddress);
+            var dnsIpAddress = Dns.GetHostEntry(Dns.GetHostName()).AddressList
+                .First(o => o.AddressFamily == AddressFamily.InterNetwork).ToString();
+
+            var registration = new AgentServiceRegistration()
+            {
+                ID = $"{consulConfig.Value.ServiceId}-{uri.Port}",
+                Name = consulConfig.Value.ServiceName,
+                Address = dnsIpAddress,
+                Port = uri.Port,
+                Tags = consulConfig.Value.Tags,
+                Check = new AgentServiceCheck
+                {
+                    Timeout = TimeSpan.FromSeconds(3),
+                    Interval = TimeSpan.FromSeconds(10),
+                    HTTP = $"{uri.Scheme}://{dnsIpAddress}:{uri.Port}{healthCheckPath}",
+                    Status = HealthStatus.Critical
+                }
+            };
+
+            logger.LogInformation("Registering with Consul");
+            consulClient.Agent.ServiceDeregister(registration.ID).ConfigureAwait(true);
+            consulClient.Agent.ServiceRegister(registration).ConfigureAwait(true);
+
+            lifetime.ApplicationStopping.Register(() =>
+            {
+                logger.LogInformation("Unregistering from Consul");
+                consulClient.Agent.ServiceDeregister(registration.ID).ConfigureAwait(true);
+            });
+
+            return app;
+        }
+    }
+}
---
+++ dev-kit/api.gw/Startup.cs
@@ -0,0 +1,56 @@
+using Api.Gw.ServiceDiscovery;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace Api.Gw
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile($"appsettings.json")
+                .AddJsonFile($"appsettings.{environmentName}.json", reloadOnChange: true, optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddConsul(Configuration);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseConsul();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}
