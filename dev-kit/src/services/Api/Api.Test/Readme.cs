fc8eec1963c4f0c4ebb1068618cb68623936a4a5
s.azarin <azarin.sergey@gmail.com>
2020-06-09 01:12:03 +0700

    for future - how use swagger gen for interface

---
+++ dev-kit/src/services/Api/Api.Test/Readme.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using System.Collections.Generic;
+
+namespace Api.Test
+{
+    //SwaggerGenerator
+    //SchemaGenerator
+    //Для генерации сваггера по интерфейсу надо определить IApiDescriptionGroupCollectionProvider из MVC модуля
+    //Этот интерфейс по OpenApi сделан и его сваггер использует для генерации json
+    public class Test : IApiDescriptionGroupCollectionProvider
+    {
+        public Test()
+        {
+            var apiDescriptions = new List<ApiDescription>
+            {
+                new ApiDescription//вот эта сущность используется генератором сваггера
+                {
+                    ActionDescriptor = new InterfaceMethodDescriptor(),
+                    GroupName = "ServiceInterfaceName",
+                    HttpMethod = null,//судя по коду null не поддерживается сваггером - каждый метод в интерфейсе надо помечать аттрибутом
+                    RelativePath = "path to interface endpoint",//этот путь используется сваггером как путь к контроллеру? или сразу до метода?
+                    //ParameterDescriptions = { }
+                    //Properties = { }
+                    //SupportedRequestFormats = { }
+                    //SupportedResponseTypes = { }
+                }
+            };
+
+            var apiDescGroups = new List<ApiDescriptionGroup>
+            {
+                new ApiDescriptionGroup("ServiceInterfaceName", apiDescriptions)
+            };
+
+            ApiDescriptionGroups = new ApiDescriptionGroupCollection(apiDescGroups, 0);
+        }
+        public ApiDescriptionGroupCollection ApiDescriptionGroups { get; }
+    }
+
+    public class InterfaceMethodDescriptor : ActionDescriptor
+    {
+        //непонятно пока что из этого юзается сваггером
+        public InterfaceMethodDescriptor()
+        {
+
+        }
+    }
+}
--- dev-kit/src/services/Api/Api.Test/Startup.cs
+++ dev-kit/src/services/Api/Api.Test/Startup.cs
@@ -7,7 +7,19 @@ using Microsoft.Extensions.DependencyInjection;
 using Svc.Contract.Service;
 using Svc.Implementation;
 using System;
+using System.Collections.Generic;
+using System.Text.Json;
 using Core.Service.Host.ServiceDiscovery.Proxy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.Extensions.ApiDescriptions;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace Api.Test
 {
@@ -15,13 +27,18 @@ namespace Api.Test
     {
         protected override Type[] ServiceContractTypes => Array.Empty<Type>();
 
-        public override void AddServices(IServiceCollection c)
+        public override void AddServices(IServiceCollection services)
         {
-            
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
         }
 
         public override void ServiceConfiguration(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+
             //TODO: добавить MVC роутинг
             app.UseEndpoints(endpoints =>
             {
