85deea3b9f7b670873744b9976fcd40910794bb8
s.azarin <azarin.sergey@gmail.com>
2020-06-11 01:02:30 +0700

    swagger gen assembly added

---
+++ dev-kit/src/.core/Core.Service.Host.Swagger/ServiceApiDescriptionGroupCollectionProvider.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Core.Service.Host.Swagger
+{
+    public class ServiceApiDescriptionGroupCollectionProvider : IApiDescriptionGroupCollectionProvider
+    {
+        public ServiceApiDescriptionGroupCollectionProvider()
+        { }
+
+        public ApiDescriptionGroupCollection ApiDescriptionGroups => new ApiDescriptionGroupCollection(GetGroups(), 0);
+
+        private IReadOnlyList<ApiDescriptionGroup> GetGroups()
+        {
+            return new Collection<ApiDescriptionGroup>
+            {
+                new ApiDescriptionGroup("InterfaceName", new []
+                {
+                    new ApiDescription
+                    {
+                        
+                    }
+                })
+            };
+        }
+    }
+}
