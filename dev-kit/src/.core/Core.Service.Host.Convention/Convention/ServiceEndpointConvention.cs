987362b75355e29b0f0215ff5ac680940d2c6591
LIME\Azarin.SA <azarin.sergey@gmail.com>
2021-04-16 20:17:47 +0700

    cleanup

--- dev-kit/src/.core/Core.Service.Host.Convention/Convention/ServiceEndpointConvention.cs
+++ dev-kit/src/.core/Core.Service.Host.Convention/Convention/ServiceEndpointConvention.cs
@@ -1,14 +1,10 @@
 ﻿using System;
 using System.Reflection;
-using Core.Service.Host.Convention.Configuration;
 
 namespace Core.Service.Host.Convention.Convention
 {
     public class ServiceEndpointConvention : IServiceEndpointConvention
     {
-        public string GetServiceHost(ApplicationConfig appConfig, string serviceName)
-            => $"{appConfig.ApplicationName}.{serviceName}";
-
         public string GetServiceMethodPath(MethodInfo methodInfo, Type serviceInterfaceType)
         {
             var serviceInterfaceName = serviceInterfaceType.Name;
