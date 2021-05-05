64962b3f1c4331ddc6efd55a33daf725e71b347c
sazarin <azarin.sergey@gmail.com>
2021-05-06 01:49:15 +0700

    more databases for samples

--- dev-kit/build/deploy/local/postgres/bash/server_config.sh
+++ dev-kit/build/deploy/local/postgres/bash/server_config.sh
@@ -2,6 +2,12 @@ psql -U admin -d postgres -c "create user ${POSTGRES_USER_NAME} with createdb cr
 psql -U admin -d postgres -c "create tablespace ${POSTGRES_USER_TABLESPACE} owner ${POSTGRES_USER_NAME} location '/var/lib/postgresql'"
 psql -U admin -d postgres -c "create database ${POSTGRES_USER_DB} owner=${POSTGRES_USER_NAME} tablespace=${POSTGRES_USER_TABLESPACE}"
 #
+#execute sql
+#
+for file in /tmp/sql/*.sql; do 
+	psql -U ${POSTGRES_USER_NAME} -d ${POSTGRES_USER_DB} -a -f "${file}" 
+done
+#
 #sample_db
 #
 psql -U admin -d postgres -c "create user postgres createdb createrole superuser PASSWORD '${POSTGRES_USER_PASSWORD}'"
@@ -9,8 +15,6 @@ psql -U admin -d postgres -c "grant admin to postgres"
 psql -U postgres -d postgres -c "create database otus_dvdrental_db owner=postgres tablespace=${POSTGRES_USER_TABLESPACE}"
 pg_restore -U postgres -d otus_dvdrental_db -v /sample_db/dvdrental.tar --no-password
 #
-#execute sql
+#demo db
 #
-for file in /tmp/sql/*.sql; do 
-	psql -U ${POSTGRES_USER_NAME} -d ${POSTGRES_USER_DB} -a -f "${file}" 
-done
+psql -U ${POSTGRES_USER_NAME} -d ${POSTGRES_USER_DB} -a -f "/sample_db/airport_sample.sql"
--- dev-kit/docs/README.md
+++ dev-kit/docs/README.md
@@ -8,6 +8,34 @@
    * Three 
   
 
-3. I'm using sample db:
+3. I'm using sample dvdrental db:
    ![sample_db_schema](content/db_schema.png)
+
+   And airport tickets db:
+   ![sample_db_schema](content/airport_schema.png)
+
+
+4. Bad sql to improve (airports db):
+```
+EXPLAIN ANALYZE
+SELECT  f.flight_no
+		, f.scheduled_departure::date as vilet
+		, a1.airport_name as from_air
+		, a2.airport_name as to_air
+		, count(s.seat_no)
+		, count(tf.ticket_no)
+FROM bookings.flights f
+JOIN bookings.airports as a1 
+	ON a1.airport_code = f.departure_airport
+JOIN bookings.airports as a2 
+	ON a2.airport_code = f.arrival_airport
+JOIN bookings.seats as s 
+	ON s.aircraft_code = f.aircraft_code
+LEFT JOIN bookings.ticket_flights tf 
+	ON tf.flight_id = f.flight_id
+GROUP BY f.flight_id, f.flight_no, vilet, from_air, to_air
+ORDER BY vilet
+LIMIT 10;
+```
+
   
